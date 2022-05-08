8f56ee2e3e6a66044832e5bbe7a14cf66ba0b210
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-05-08 18:19:57 +0200

    CukesRunner added features and glue

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -1,4 +1,13 @@
 package com.vytrack.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/vytrack/step_definations"
+)
 public class CukesRunner {
 }
