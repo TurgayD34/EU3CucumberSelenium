aa361973de53a0ab0f3ca1962d75dd9abde241a8
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-05-09 11:43:52 +0200

    Cukes Runner

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -7,7 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com/vytrack/step_definations"
+            glue = "com/vytrack/step_definations",
+            dryRun = false,
+            tags = "@navigation"
+        // dryRun false execute the real code  and dryRun true will not execute the java code inside the step def
+        // if use "and" just execute a scenario a tag, and if use "or" can execute two or more testcases at the same time.
 )
 public class CukesRunner {
 }
