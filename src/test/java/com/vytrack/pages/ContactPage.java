8b168f938c7e5f1ce70dbec55ceeb6b9e3c36025
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-05-11 17:22:53 +0200

    Contact Page

---
+++ src/test/java/com/vytrack/pages/ContactPage.java
@@ -0,0 +1,17 @@
+package com.vytrack.pages;
+
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ContactPage extends BasePage {
+
+    public WebElement getContactEmail(String email) {
+        String xpath = "//td[contains(text(),'" + email + "') and @data-column-label='Email']";
+        return Driver.get().findElement(By.xpath(xpath));
+    }
+
+    @FindBy(css = ".input-widger")
+    public WebElement pageNumber;
+}
