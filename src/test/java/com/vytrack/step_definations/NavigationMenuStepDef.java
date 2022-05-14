b1b97db16cd711d6062457959c2161d225f128dc
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-05-14 04:29:27 +0200

    navigation Menu Step Def

--- src/test/java/com/vytrack/step_definations/NavigationMenuStepDef.java
+++ src/test/java/com/vytrack/step_definations/NavigationMenuStepDef.java
@@ -1,7 +1,11 @@
 package com.vytrack.step_definations;
 
+import com.vytrack.pages.ContactPage;
+import com.vytrack.pages.DashboardPage;
+import com.vytrack.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class NavigationMenuStepDef {
 
@@ -48,5 +52,20 @@ public class NavigationMenuStepDef {
     @Then("expected title is calender events")
     public void expected_title_is_calender_events() {
         System.out.println("Calender events page");
+    }
+
+    @When("the user navigates to {string} {string}")
+    public void the_user_navigates_to(String tab, String module) {
+        DashboardPage dashboardPage = new DashboardPage();
+//        dashboardPage.waitUntilLoaderScreenDisappear();
+        dashboardPage.navigateToModule(tab, module);
+    }
+    @Then("default page number should be {int}")
+    public void default_page_number_should_be(Integer pageNum) {
+        BrowserUtils.waitFor(3);
+        ContactPage contactPage = new ContactPage();
+        contactPage.waitUntilLoaderScreenDisappear();
+        Integer actualNumber = Integer.parseInt(contactPage.pageNumber.getAttribute("value"));
+        Assert.assertEquals(pageNum, actualNumber);
     }
 }
