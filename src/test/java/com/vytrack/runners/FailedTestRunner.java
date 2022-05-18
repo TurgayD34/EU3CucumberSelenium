a42846dee05bcb2bcbae18b481475fd1a6e7576e
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-05-18 16:23:11 +0200

    Failed Test Runner

---
+++ src/test/java/com/vytrack/runners/FailedTestRunner.java
@@ -0,0 +1,20 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/failed-html-report"},
+        features = "@target/rerun.txt",
+        glue = "com/vytrack/step_definations"
+
+
+)
+
+public class FailedTestRunner {
+
+
+
+}
