b8c3ce813cc4b6c3195df0f5dea7f3011e2257e5
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-05-29 13:13:45 +0200

    Cukes Runner

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -6,7 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber.json"},
+        plugin = {"json:target/cucumber.json",
+                  "html:target/default-html-reports",
+                  "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
             glue = "com/vytrack/step_definations",
             dryRun = false,
